5876411fcd6312e6bc8b67b848e8a7300c0b317c
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-20 09:24:53 +0300

    created the student model

--- models/student.js
+++ models/student.js
@@ -1,31 +1,43 @@
+//- import the 'mongoose'
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/warmup_std");
 
-//- import the 'mongoose' 
-
-
+const db = mongoose.connection;
 
 // The new schema, here
-    //- Create new Schema called 'studentSchema'
-    // * firstName - (String, required)
-    // * lastName - (String, required)
-    // * grade - (Number, required)
-    // * age - (Number, Greater than or equal 18)
-    // * city - (String)
+//- Create new Schema called 'studentSchema'
+// * firstName - (String, required)
+// * lastName - (String, required)
+// * grade - (Number, required)
+// * age - (Number, Greater than or equal 18)
+// * city - (String)
 
+const Schema = mongoose.Schema;
 
+const studentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  grade: {
+    type: Number,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: 18
+  },
+  city: String
+});
 
+// The model of the schema
+const Student = mongoose.model("Student", studentSchema);
 
-
-
-
-
-
-
-    
-
-
-// The model of the schema 
-
-
-
-
-// Export the model 
+// Export the model
+module.exports = {
+  Student
+};
